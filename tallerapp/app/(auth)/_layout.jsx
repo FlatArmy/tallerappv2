8f8586965b3a7c7275e15fa9c9ad87c529d9e30d
alex2006 <2006.alex@gmail.com>
2024-08-20 02:28:01 -0500

    first commit

---
+++ tallerapp/app/(auth)/_layout.jsx
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const Auhtlayout = () => {
+  return (
+    <>
+    <Stack>
+      <Stack.Screen
+        name="sign-in"
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen
+        name="sign-up"
+        options={{
+          headerShown: false
+        }}
+      />
+    </Stack>
+    </>
+  )
+}
+
+export default Auhtlayout
